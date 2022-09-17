9757e92afc2f207ef78a41c83662ca3dad106833
johnstennett24 <93450112+johnstennett24@users.noreply.github.com>
2022-09-17 14:07:54 -0600

    added C++ code

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::string name;
+    std::cout << "Please enter your name and press enter.";
+    std::cin >> name;
+
+    std::cout << name + ", welcome to this repo! I hope you enjoy your time here.";
+
+}
